f694e0a170537a87a42324cd181b9effbbc6a3bf
izygabs <80975143+izygabs@users.noreply.github.com>
2024-03-18 12:18:31 +0100

    Deploy SignupPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Epp from "./Entrypoints/test";
+import SignUp from "./Entrypoints/SignupPage";
 
 function App() {
   return (
     <>
       <Epp />
+      <br />
+      <br />
+      <SignUp />
     </>
   );
 }
