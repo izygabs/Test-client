4c59c991ee805280a2af1b5c76e666426ec20015
izygabs <80975143+izygabs@users.noreply.github.com>
2024-03-18 12:36:36 +0100

    Deploy Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import React from "react";
-import Epp from "./Entrypoints/test";
-import SignUp from "./Entrypoints/SignupPage";
+import "./App.css";
+
+// import SideNav from "./components/SideNav";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Dashboard from "./pages/Dashboard";
+// import Assignment from "./pages/Assignment";
+// import Courses from "./pages/Courses";
+// import Settings from "./pages/Settings";
+// import Reports from "./pages/Reports";
+import SignupPage from "./EntryPoint/SignupPage";
+// import LoginPage from "./EntryPoint/LoginPage";
+import LandingPage from "./EntryPoint/LandingPage";
+// import GooglePage from "./EntryPoint/GooglePage";
+// import RegistrationForm from "./components/RegistrationForm";
+// import UnsuccessfulPopUp from "./components/UnsuccessfulPopup";
+// import SuccessPopUp from "./components/successpopup";
 
 function App() {
+  // const [openNav, setOpenNav] = useState(false);
+  // const open = () => {
+  //   setOpenNav(!openNav);
+  // };
+  // const close = () => {
+  //   setOpenNav(!openNav);
+  // };
+
   return (
-    <>
-      <Epp />
-      <br />
-      <br />
-      <SignUp />
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* <Route path="/google-signup" element={<GooglePage />} /> */}
+          {/* <Route path="/registration" element={<RegistrationForm />} /> */}
+          {/* <Route path="/successfulreg" element={<SuccessPopUp />} /> */}
+          {/* <Route path="/unsuccessfulreg" element={<UnsuccessfulPopUp />} /> */}
+          {/* <Route
+            path="/dashboard/*"
+            element={
+              <SideNav toggle={open} openNav={openNav} close={close}>
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/assignment" element={<Assignment />} />
+                  <Route
+                    path="/courses"
+                    element={<Courses openNav={openNav} close={close} />}
+                  />
+                  <Route path="/reports" element={<Reports />} />
+                  <Route path="/settings" element={<Settings />} />
+                </Routes>
+              </SideNav>
+            }
+          /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 export default App;
